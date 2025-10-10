00648eac624c5798143d900569d46a0b9d3569ee
lanreoyekanmi <olanrewajuoyekanmi241@gmail.com>
2025-10-10 06:49:04 +0100

    build

--- src/types/css-modules.d.ts
+++ src/types/css-modules.d.ts
@@ -27,3 +27,18 @@ declare module '*.svg' {
   const value: string;
   export default value;
 }
+
+declare module '*.mp4' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.webm' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.ogg' {
+  const value: string;
+  export default value;
+}
